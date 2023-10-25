72c76094b6ce7db0ebd73f6dd15284db97b0827d
dabidgmz <david.gmzherrera28@gmail.com>
2023-10-24 20:09:02 -0600

    ou

--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,6 +1,6 @@
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import Usuario from 'App/Models/Usuario'
-import UserValidationValidator from 'app/Validators/UserValidationValidator';
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Usuario from 'App/Models/Usuario';
+import UserValidationValidator from 'App/Validators/UserValidationValidator';
 export default class UsuariosController {
     public async index({ response }: HttpContextContract) {
         const usuarios = await Usuario.query().preload('posts').preload('comments') 
@@ -27,64 +27,69 @@ export default class UsuariosController {
         }
     }
     */
+
         public async tipobusq({ request, response }: HttpContextContract) {
-        const tipo = request.method()
-        switch (tipo) {
+          const tipo = request.method();
+          switch (tipo) {
             case 'GET':
-    try {
-        const userId = request.param('id');
-        if (userId) {
-            const usuario = await Usuario.find(userId);
-            if (usuario) {
-                return response.status(200).json({ message: 'Usuario encontrado con éxito', usuario });
-            } else {
-                return response.status(404).json({ message: 'Usuario no encontrado' });
-            }
-        } else {
-            const usuarios = await Usuario.all();
-            return response.status(200).json({ message: 'Lista de usuarios', usuarios });
-        }
-    } catch (error) {
-        return response.status(500).json({ message: 'Error interno del servidor', error: error.message });
-    }
-            case 'POST':
-            const validatedData = await request.validate(UserValidationValidator);
-            const userData = request.only(['name', 'email', 'password', 'phone', 'status']);
-            const newUser = await Usuario.create(userData);
-            return response.status(201).json(newUser);
-            case 'PUT':
-            try {
-                const validatedData = await request.validate(UserValidationValidator);
+              try {
                 const userId = request.param('id');
+                if (userId) {
+                  const usuario = await Usuario.find(userId);
+                  if (usuario) {
+                    return response.status(200).json({ message: 'Usuario encontrado con éxito', usuario });
+                  } else {
+                    return response.status(404).json({ message: 'Usuario no encontrado' });
+                  }
+                } else {
+                  const usuarios = await Usuario.all();
+                  return response.status(200).json({ message: 'Lista de usuarios', usuarios });
+                }
+              } catch (error) {
+                return response.status(500).json({ message: 'Error interno del servidor', error: error.message });
+              }
+            case 'POST':
+              try {
+                const validatedData = await request.validate(UserValidationValidator);
+                const userData = request.only(['name', 'email', 'password', 'phone', 'status']);
+                const newUser = await Usuario.create(userData);
+                return response.status(201).json(newUser);
+              } catch (error) {
+                return response.status(400).json({ message: 'Error al crear el usuario', error: error.messages });
+              }
+            case 'PUT':
+              try {
+                const userId = request.param('id');
+                const validatedData = await request.validate(UserValidationValidator);
                 const updatedData = request.only(['name', 'email', 'password', 'phone', 'status']);
                 const userToUpdate = await Usuario.find(userId);
                 if (userToUpdate) {
-                userToUpdate.merge(updatedData);
-                await userToUpdate.save();
-                return response.status(200).json({ message: 'Usuario actualizado con éxito', usuario: userToUpdate });
+                  userToUpdate.merge(updatedData);
+                  await userToUpdate.save();
+                  return response.status(200).json({ message: 'Usuario actualizado con éxito', usuario: userToUpdate });
                 } else {
-                return response.status(404).json({ message: 'Usuario no encontrado' });
+                  return response.status(404).json({ message: 'Usuario no encontrado' });
                 }
-            } catch (error) {
-                return response.status(500).json({ message: 'Error al actualizar el usuario', error: error.message });
-            }
+              } catch (error) {
+                return response.status(400).json({ message: 'Error al actualizar el usuario', error: error.messages });
+              }
             case 'DELETE':
-            try {
+              try {
                 const userIdToDelete = request.param('id');
                 const userToDelete = await Usuario.find(userIdToDelete);
                 if (userToDelete) {
-                userToDelete.status = 'inactive';
-                await userToDelete.save();
-                return response.status(200).json({ message: 'Usuario desactivado con éxito' });
+                  userToDelete.status = 'inactive';
+                  await userToDelete.save();
+                  return response.status(200).json({ message: 'Usuario desactivado con éxito' });
                 } else {
-                return response.status(404).json({ message: 'Usuario no encontrado' });
+                  return response.status(404).json({ message: 'Usuario no encontrado' });
                 }
-            } catch (error) {
+              } catch (error) {
                 return response.status(500).json({ message: 'Error al desactivar el usuario', error: error.message });
-            }
+              }
             default:
-            return response.status(400).json({ message: 'Mira bien soy el mencho y este método no admitido ala otra fijate bien si no amaneces de plomo' });
+              return response.status(400).json({ message: 'Mira bien soy el mencho y este método no admitido, fíjate bien si no amaneces de plomo' });
+          }
         }
-    }
-}
-
+      }
+      
--- app/Validators/UserValidationValidator.ts
+++ app/Validators/UserValidationValidator.ts
@@ -12,7 +12,7 @@ export default class UserValidationValidator {
     email: schema.string({}, [
       rules.email(),
       rules.maxLength(255),
-      rules.unique({ table: 'users', column: 'email' }),
+      rules.unique({ table: 'usuarios', column: 'email' }), // Cambiado 'users' a 'usuarios'
     ]),
     password: schema.string({}, [
       rules.minLength(8),
@@ -28,16 +28,16 @@ export default class UserValidationValidator {
   });
 
   public messages: CustomMessages = {
-    'name.required': 'El nombre es obligatorio.',
-    'name.minLength': 'El nombre debe tener al menos {{ options.minLength }} caracteres.',
-    'name.maxLength': 'El nombre no puede tener más de {{ options.maxLength }} caracteres.',
-    'email.required': 'El correo electrónico es obligatorio.',
-    'email.email': 'El correo electrónico no tiene un formato válido.',
+    'name.required': 'ey te crees gracioso? el nombre es obligatorio!!.',
+    'name.minLength': 'El nombre debe tener al menos {{ options.minLength }} caracteres ponte al tiro mijo.',
+    'name.maxLength': 'El nombre no puede tener más de {{ options.maxLength }} caracteres en que mundo vives?.',
+    'email.required': 'El correo electrónico es obligatorio si no a donde te legan mi mensajes bb.',
+    'email.email': 'El correo electrónico no tiene un formato válido pilas mijo.',
     'email.maxLength': 'El correo electrónico no puede tener más de {{ options.maxLength }} caracteres.',
-    'email.unique': 'Este correo electrónico ya está en uso.',
-    'password.required': 'La contraseña es obligatoria.',
-    'password.minLength': 'La contraseña debe tener al menos {{ options.minLength }} caracteres.',
+    'email.unique': 'Este correo electrónico ya está en uso ya ni la riegas men .',
+    'password.required': 'a donde tan rapido velosista? La contraseña es obligatoria .',
+    'password.minLength': 'La contraseña debe tener al menos {{ options.minLength }} caracteres si no te hackeo.',
     'password.maxLength': 'La contraseña no puede tener más de {{ options.maxLength }} caracteres.',
-    'phone.maxLength': 'El número de teléfono no puede tener más de {{ options.maxLength }} caracteres.',
+    'phone.maxLength': 'El número de teléfono no puede tener más de {{ options.maxLength }} caracteres o que tienes movistar eda.',
   };
 }
